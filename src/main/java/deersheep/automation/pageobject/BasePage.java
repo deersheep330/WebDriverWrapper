3b0065d9e5eb7ab3cdca68045d8600f76640dd3f
YangChiu <yangchiu@inquartik.com>
2020-09-01 14:03:20 +0800

    add operation in BasePage

--- src/main/java/deersheep/automation/pageobject/BasePage.java
+++ src/main/java/deersheep/automation/pageobject/BasePage.java
@@ -1,6 +1,7 @@
 package deersheep.automation.pageobject;
 
 import deersheep.automation.element.Element;
+import deersheep.automation.operation.Operation;
 import org.openqa.selenium.WebDriver;
 
 import java.util.HashMap;
@@ -9,10 +10,12 @@ import java.util.Map;
 public class BasePage {
 
     protected WebDriver driver;
+    protected Operation op;
     protected Map<String, Element> elements = new HashMap<>();
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.op = new Operation(this.driver);
     }
 
     protected void addElement(String name, String xpath) {
