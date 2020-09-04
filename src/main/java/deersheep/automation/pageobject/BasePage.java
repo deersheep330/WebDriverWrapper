aacfb9ce89ac0783cd78623eca5aff9fcd64895d
YangChiu <yangchiu@inquartik.com>
2020-09-04 08:52:07 +0800

    add url methods

--- src/main/java/deersheep/automation/pageobject/BasePage.java
+++ src/main/java/deersheep/automation/pageobject/BasePage.java
@@ -12,6 +12,7 @@ public class BasePage {
     protected WebDriver driver;
     protected Operation op;
     protected Map<String, Element> elements = new HashMap<>();
+    protected String url;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
@@ -23,5 +24,14 @@ public class BasePage {
     }
 
     protected Element getElement(String name) { return elements.get(name); }
+
+    public void setUrl(String url) { this.url = url; }
+
+    public String getUrl() { return url; }
+
+    public void navigate() {
+        if (url == null) throw new RuntimeException("url is null! please set it in constructor or using setUrl method.");
+        op.navigateTo(url);
+    }
 
 }
