1f39df49239130e85a65ee69fe29bccab480e5bc
YangChiu <yangchiu@inquartik.com>
2020-09-08 11:01:24 +0800

    revert change

--- src/main/java/deersheep/automation/operation/Operation.java
+++ src/main/java/deersheep/automation/operation/Operation.java
@@ -637,7 +637,7 @@ public class Operation {
     }
 
     public void scrollToElement(Element target) {
-        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", findElement(target));
+        js.executeScript("arguments[0].scrollIntoView();", findElement(target));
     }
 
     public void quitAndCloseBrowser() {
