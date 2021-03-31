4e05983f04c00e022cb880dfb5d744208a2f6727
YangChiu <yangchiu@inquartik.com>
2021-03-31 10:36:20 +0800

    add parsing float method

--- src/main/java/deersheep/automation/utility/NumberTool.java
+++ src/main/java/deersheep/automation/utility/NumberTool.java
@@ -12,6 +12,7 @@ public class NumberTool {
 
     protected static Pattern numberPattern = Pattern.compile("[-,0-9]+");
 
+    protected static Pattern floatPattern = Pattern.compile("[-,.0-9]+");
 
     public static String findIntFromString(String str) {
         Matcher matcher = numberPattern.matcher(str);
@@ -24,6 +25,23 @@ public class NumberTool {
     public static Integer parseIntFromString(String str) {
         try {
             return numberFormat.parse(findIntFromString(str)).intValue();
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static String findFloatFromString(String str) {
+        Matcher matcher = floatPattern.matcher(str);
+        while(matcher.find()) {
+            str = matcher.group();
+        }
+        return str;
+    }
+
+    public static Float parseFloatFromString(String str) {
+        try {
+            return numberFormat.parse(findFloatFromString(str)).floatValue();
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
